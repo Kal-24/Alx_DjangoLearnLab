f340ec3545a45ac99d4addb394ea15f39eb1fc1b
Kal-24 <kaleabmengesha060@gmail.com>
2025-09-05 16:35:06 +0300

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,4 +30,30 @@ def register_view(request):
         form = UserCreationForm()  # checker expects this exact instantiation
 
     return render(request, 'relationship_app/register.html', {'form': form})  # checker expects this template path
+    # views.py
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
