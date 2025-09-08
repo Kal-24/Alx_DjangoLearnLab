7211c7c1618528372475af80cfc2bb3acdc6eef9
Kal-24 <kaleabmengesha060@gmail.com>
2025-09-08 09:49:10 +0300

    models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -19,4 +19,20 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+    # Add other fields if needed
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
