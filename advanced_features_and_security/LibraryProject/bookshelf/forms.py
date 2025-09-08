ef30c3d4767f03a60cf7e638fd4887291b24f60e
Kal-24 <kaleabmengesha060@gmail.com>
2025-09-08 10:01:42 +0300

    forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,20 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=True)
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter book title'})
+    )
+    author = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter author name'})
+    )
+    publish_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
